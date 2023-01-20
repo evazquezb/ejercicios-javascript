c9f237fb0f03a8f7a665e6eb264eff6c5fe1b7d5
erayde <nosenose1434@gmail.com>
2023-01-20 02:01:05 -0800

    agregado ejercicio bloques y agua

--- interfaz/interfaz.js
+++ interfaz/interfaz.js
@@ -2,13 +2,43 @@
 //Elementos en el documento html;
 const inputCantidad = document.getElementById('inputCantidad');
 const resultado = document.getElementById('sectionResultadoCantidad');
+const resultadoBloques = document.getElementById('sectionResultadoBloques');
 //Arreglos para usar como tipo de moneda;
 const moneda =["pesos ","centavos"];
 //objeto convertidor importado de modulo;
 import {convertidor} from "../modulos/cantidadAtexto.js";
 //objeto credor de matriz con bloques importdo de modulo;
 import { creadorBloques } from "../modulos/bloques.js";
-//Manejador del evento click del boton;
+
+//FUNCIONES
+//Funcion para agregar manejador a los range creadores de bloques
+const agregarManejadorAbloquesRanges = () => {
+   const bloquesRanges = document.querySelectorAll("#br1");
+   bloquesRanges.forEach(range=>range.addEventListener('change',dibujarBloques.bind(bloquesRanges)));
+} 
+//Funcion para dibujar matriz con bloques y agua, medianto el objeto creadorBloques importado del modulo bloques.
+const dibujarBloques = function(){
+    const valores = [];
+    resultadoBloques.innerHTML="";
+    resultadoBloques.childNodes.forEach(node=>console.log(node));
+    this.forEach(range=>valores.push(range.value));
+    const matriz = creadorBloques.crear(valores);
+    matriz.forEach((fila,index)=>{
+        const flexRow=document.createElement("div");
+        flexRow.setAttribute("class","fila"); 
+        fila.forEach((columna,idx)=>{
+            const bloque = document.createElement("div");
+            matriz[index][idx]=='██' ? bloque.setAttribute("class","bloque") :
+            matriz[index][idx]=='~~' ? bloque.setAttribute("class","agua") : null;
+            if(matriz.length>2 && index==matriz.length-3 && idx==6 && matriz[index][idx]!='██')
+                matriz[index][idx]==null ? null:bloque.setAttribute("class","solOff");
+            flexRow.appendChild(bloque); 
+        });
+        resultadoBloques.appendChild(flexRow);
+    })
+}
+//MANEJADORES de eventos
+//Manejador del evento input del input cantidad pra convertir a letras;
 inputCantidad.addEventListener('input',()=>{
     //usamos el método "convertirAtexto" del objeto importado "convertidor"
     //pasando como argumento la cantidad del intput number del documento
@@ -17,3 +47,6 @@ inputCantidad.addEventListener('input',()=>{
     if(texto)resultado.innerHTML+="<font color='#f66'>"+texto.pesos+moneda[0] + " con " + texto.centavos+moneda[1] + "</font>";
     //cantidad.value="";
 });
+
+window.addEventListener("load",agregarManejadorAbloquesRanges);
+

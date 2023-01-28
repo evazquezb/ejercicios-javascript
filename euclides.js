9bc725947bc32bbcb41a243813735f3fc2d797e9
erayde <nosenose1434@gmail.com>
2023-01-27 17:16:56 -0800

    cambiados nombres de parametros

--- euclides.js
+++ euclides.js
@@ -1,8 +1,8 @@
 //implementamos el algoritmo de Euclides par obtener el máximo común divisor.
-const Euclides = (ancho,alto) => alto==0 ? ancho : Euclides(alto,ancho % alto);
+const Euclides = (numerador,denominador) => denominador==0 ? numerador : Euclides(denominador,numerador % denominador);
 //creamos una función que divida ambos términos de la fracción entre el máximo común divisor
 //y retorne un arreglo con los dos elementos divididos.
-const reducirFraccion = (ancho,alto) =>{    
-    const mcd = Euclides(ancho,alto);
-    return [ancho/mcd,alto/mcd]};
+const reducirFraccion = (numerador,denominador) =>{    
+    const mcd = Euclides(numerador,denominador);
+    return [numerador/mcd,denominador/mcd]};
 //reducirFraccion(1920,1080) -> [16,9];
